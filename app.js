ae0ef57b5746d0e89255f425ec700f6864bfbd2c
aaron592 <bsilent590@gmail.com>
2021-01-25 18:03:17 +0530

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+let items = ["Bible reading" , "Learning" , "Exercise"];
+const app = express();
+
+app.set('view engine', 'ejs');
+
+let ejs = require('ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req, res){
+    
+    let today = new Date();
+    let options = {
+        weekday: "long" ,
+        day: "numeric",
+    month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
+   
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
